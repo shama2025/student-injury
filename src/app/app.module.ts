be603707f620e3ffa967df350ddcb929d5a38ba9
shama2025 <2003mashaffer@gmail.com>
2023-12-15 00:04:18 -0500

    Update imports in app.module
    * FeaturesHTTP Client Module
    * Login page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, LoginPageComponent],
+    imports: [BrowserModule, AppRoutingModule,HttpClientModule],
     providers: [],
     bootstrap: [AppComponent]
 })
