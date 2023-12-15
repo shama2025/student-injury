7298e80532c82430d096a052afffbc8c497f816c
Marcus Shaffer <101298098+shama2025@users.noreply.github.com>
2023-12-15 00:16:57 -0500

    Create login page (#20)
    * Move app.py
    
    Moves to backend folder
    
    * Update .gitignore
    
    Includes pycache
    
    * Create util file
    
    This will hold python functions
    
    * Add run api script
    
    * Add login-page selector
    
    * Update imports in app.module
    
    * FeaturesHTTP Client Module
    * Login page component
    
    * Create login page service
    
    Purpose: communicate to api
    
    * Create service
    
    Communicate to api
    
    * Create login page css
    
    * Create login page html
    
    * Run Style checks
    
    * Update app.py
    
    Fixes api test error
    
    * Update reqiurements.txt
    
    * Add reqiurements.txt install

---
+++ backend/api_util.py
@@ -0,0 +1,5 @@
+"""This file will contain functions related to the API"""
+
+
+def confirmLoginCredentials():
+    return ""
--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,20 @@
 """This app.py file will hold the api for the Angular application"""
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app)
 """This route will act as an api endpoint for the login page"""
 
 
 @app.route("/api/login")
 def api_login():
-    return ""
+    username = request.args.get("username")
+    password = request.args.get("password")
+    if username == None or password == None:
+        return {"Error": "No username or password entered"}, 404
+    return {"Username": username, "Password": password}, 200
 
 
 """This route will act as an api endpoint for new account creation"""
