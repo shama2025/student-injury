c2d41928fdc8ba544cc05fbc17ebbb66fcc92f06
shama2025 <2003mashaffer@gmail.com>
2023-12-17 21:24:56 -0500

    Update app.py
    * Update imports from api_util
    * Update confirm_login_credentials naming scheme
    * Update /api/login return objects
    * Create functionality for /api/new/account endpoint

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 """This app.py file will hold the api for the Angular application"""
 from flask import Flask, request, Response
 from flask_cors import CORS
-from backend_util.api_util import confirmLoginCredentials
+from backend_util.api_util import (
+    confirm_login_credentials,
+    add_new_user,
+    check_user_exists,
+)
 
 
 app = Flask(__name__)
@@ -17,10 +21,14 @@ def api_login():
         return Response(
             "No username or password entered", content_type="text/plain", status=404
         )
-    confirmLogin = confirmLoginCredentials(username, password)
+    confirmLogin = confirm_login_credentials(username, password)
     if not confirmLogin:  # If user doesn't exist
-        return Response("No", status=404)  # Returns a user not found
-    return Response("Yes", status=200)  # Returns ok when user is found
+        return Response(
+            "User not Found!", status=404, content_type="text/plain"
+        )  # Returns a user not found
+    return Response(
+        "User is found!", status=200, content_type="text/plain"
+    )  # Returns ok when user is found
 
 
 """This route will act as an api endpoint for new account creation"""
@@ -28,7 +36,18 @@ def api_login():
 
 @app.route("/api/new/account")
 def api_new_account():
-    return ""
+    username = request.args.get("username")
+    password = request.args.get("password")
+    email = request.args.get("email")
+    name = request.args.get("name")
+
+    new_user_list = (username, password, email, name) #Creates new user list
+
+    if check_user_exists(new_user_list):
+        return Response("User already exists!", status=404, content_type="text/plain") #Returns 404 if user already exists
+    if add_new_user(new_user_list):
+        return Response("User Created!", status=200, content_type="text/plain") #Returns 200 for an ok Response
+    return Response("Error when creating user!", status=500, content_type="text/plain") #Returns a 500 if an error occurs
 
 
 """This route will act as an api endpoint for AT email communication"""
