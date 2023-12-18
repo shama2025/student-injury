4b33f01e91cbcd0fc3cb49d2e3ac7cd0f53f559a
shama2025 <2003mashaffer@gmail.com>
2023-12-18 12:26:20 -0500

    Create tests for ui
    * Tests for new account for existing user
    * Tests for new account for new user

--- test_ui.py
+++ test_ui.py
@@ -1,46 +1,63 @@
 """This test_ui.py file will hold all of the UI tests for this application"""
 import random
 import string
+from faker import Faker
 from playwright.sync_api import Page, expect
 
+fake = Faker()
+Faker.seed(42)
 
-# Update these tests as the project gets more defined
+#Update this test when injury form page is created
 def test_login_existing_user_ui(page: Page):
     """This test will login in an existing user and confirm the console log shows "Hooray"""
-    console_logs = []
     page.goto("http://localhost:4200/")
     page.locator('input[name="username-input"]').click()
     page.locator('input[name="username-input"]').fill("JohnDoe123")
     page.locator('input[name="password"]').click()
     page.locator('input[name="password"]').fill("password123")
     page.get_by_role("button", name="Sign In!").click()
-    page.on(
-        "console", lambda console_message: console_logs.append(console_message.text())
-    )
-    assert ("Hooray" in log for log in console_logs)
+    #expect(page.url == "")#change url to injury forms page
 
 
-# Update these tests as the project gets more defined
+#Update this test when injury form page is created
 def test_login_non_existent_user_ui(page: Page):
     """This test will login in an non-existent user and check console log shows "Boo" """
     random_string = "".join(random.choices(string.ascii_letters + string.digits, k=10))
-    console_logs = []
     page.goto("http://localhost:4200/")
-    page.locator('input[name="username-input"]').click()
-    page.locator('input[name="username-input"]').fill(random_string)
-    page.locator('input[name="password"]').click()
-    page.locator('input[name="password"]').fill(random_string)
+    page.locator('app-login-page input[name="username-input"]').click()
+    page.locator('app-login-page input[name="username-input"]').fill(random_string)
+    page.locator('app-login-page input[name="password"]').click()
+    page.locator('app-login-page input[name="password"]').fill(random_string)
     page.get_by_role("button", name="Sign In!").click()
-    page.on(
-        "console", lambda console_message: console_logs.append(console_message.text())
-    )
-    assert ("Boo" in log for log in console_logs)
+    #expect warning to be on login page
 
+#Update this test when injury form page is created
+def test_create_new_user(page:Page):
+    page.goto("http://localhost:4200/")
+    page.get_by_role("button", name="New User").first.click()
+    page.locator("app-sign-up-page input[name=\"username-input\"]").click()
+    page.locator("app-sign-up-page input[name=\"username-input\"]").fill(fake.user_name())
+    page.locator("app-sign-up-page input[name=\"username-input\"]").press("Tab")
+    page.locator("app-sign-up-page input[name=\"password\"]").fill(fake.password())
+    page.locator("app-sign-up-page input[name=\"password\"]").press("Tab")
+    page.locator("input[name=\"email\"]").fill(fake.email())
+    page.locator("input[name=\"email\"]").press("Tab")
+    page.locator("input[name=\"name\"]").fill(fake.name())
+    page.get_by_role("button", name="Create Account!").click()
+    #expect(page.url == "") #change to url of form page
 
-def test_create_new_user():
-    """This test will confirm that a new user is created"""
-    return ""
-
-
-def test_user_already_exists():
+#Update this test when injury form page is created
+def test_user_already_exists(page:Page):
     """This test will confirm that a user already exists when it is trying to be created"""
+    page.goto("http://localhost:4200/")
+    page.get_by_role("button", name="New User").first.click()
+    page.locator("app-sign-up-page input[name=\"username-input\"]").click()
+    page.locator("app-sign-up-page input[name=\"username-input\"]").fill("JohnDoe123")
+    page.locator("app-sign-up-page input[name=\"username-input\"]").press("Tab")
+    page.locator("app-sign-up-page input[name=\"password\"]").fill("password123")
+    page.locator("app-sign-up-page input[name=\"password\"]").press("Tab")
+    page.locator("input[name=\"email\"]").fill("JohnDoe@test.com")
+    page.locator("input[name=\"email\"]").press("Tab")
+    page.locator("input[name=\"name\"]").fill("John Doe")
+    page.get_by_role("button", name="Create Account!").click()
+    #Expect error to appear on screen
