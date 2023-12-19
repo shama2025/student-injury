be171caebf0b78ec88b301bb37b3f9b411881ce3
shama2025 <2003mashaffer@gmail.com>
2023-12-19 10:22:42 -0500

    Create porm component

---
+++ src/app/patient-outcome-reported-measure-table-page/patient-outcome-reported-measure-table-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-outcome-reported-measure-table-page',
+  templateUrl: './patient-outcome-reported-measure-table-page.component.html',
+  styleUrls: ['./patient-outcome-reported-measure-table-page.component.css']
+})
+export class PatientOutcomeReportedMeasureTablePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
