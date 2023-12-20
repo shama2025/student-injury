fff373cc7c635ddddc86a029d2b5b06a5c0ce68b
shama2025 <2003mashaffer@gmail.com>
2023-12-20 11:36:32 -0500

    Update porm component
    * Create connection to json file
    * Create search function
    * Create filter button for ASC/DESC
    * Create filter button for filter by Injury
    * Create filter button for filter if form can be done online or completed with pen and paper
    * Import http client

--- src/app/patient-outcome-reported-measure-table-page/patient-outcome-reported-measure-table-page.component.ts
+++ src/app/patient-outcome-reported-measure-table-page/patient-outcome-reported-measure-table-page.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
-  selector: 'app-patient-outcome-reported-measure-table-page',
-  templateUrl: './patient-outcome-reported-measure-table-page.component.html',
-  styleUrls: ['./patient-outcome-reported-measure-table-page.component.css']
+    selector: 'app-patient-outcome-reported-measure-table-page',
+    templateUrl: './patient-outcome-reported-measure-table-page.component.html',
+    styleUrls: ['./patient-outcome-reported-measure-table-page.component.css']
 })
 export class PatientOutcomeReportedMeasureTablePageComponent implements OnInit {
+    constructor(private http: HttpClient) {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.createTable();
+    }
 
-  ngOnInit(): void {
-  }
+    tableForms: any;
 
+    /**
+     * This function will generate a table given x amount of PORM forms
+     */
+    createTable(): void {
+        this.http.get('/assets/media.json').subscribe(response => {
+            this.tableForms = response;
+        });
+    }
+
+    /**
+     * Searches the table for value contained in the search bar
+     */
+    search() :void{
+
+    }
+
+    /**
+     * Filters the table by type of injury 
+     */
+    filterInjuryBtn() :void{
+
+    }
+
+    /**
+     * Filters if the file can be online or needs to be done by paper pencil
+     */
+    filterIsPenPaper() :void{
+
+    }
+
+    /** 
+     * Filters by Ascending or Descending alphabetically
+     */
+    filterASCDESC() :void{
+
+    }
 }
