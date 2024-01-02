43a75f69ac8acbde33566364302a4bf497a8b15d
shama2025 <2003mashaffer@gmail.com>
2024-01-02 14:09:07 -0500

    Add routing to porm page

--- src/app/athletic-trainer-email-form/athletic-trainer-email-form.component.ts
+++ src/app/athletic-trainer-email-form/athletic-trainer-email-form.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AthleticTainerEmailServiceService } from '../services/athletic-tainer-email-service.service';
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-athletic-trainer-email-form',
     templateUrl: './athletic-trainer-email-form.component.html',
     styleUrls: ['./athletic-trainer-email-form.component.css']
 })
 export class AthleticTrainerEmailFormComponent implements OnInit {
-    constructor(private athleticTrainerEmailService: AthleticTainerEmailServiceService) {}
+    constructor(
+        private athleticTrainerEmailService: AthleticTainerEmailServiceService,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {}
     isVisible = true;
@@ -30,5 +34,12 @@ export class AthleticTrainerEmailFormComponent implements OnInit {
                     console.log('ERROR: ' + JSON.stringify(response));
                 }
             });
+    }
+
+    /**
+     * This button, when click goes back to the trainer form
+     */
+    backBtn(): void {
+        this.router.navigateByUrl('patient/outcome/reported/measure');
     }
 }
