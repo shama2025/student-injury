93de98f40f5ff430dc946cef56a32df15f5e985e
shama2025 <2003mashaffer@gmail.com>
2024-01-03 16:29:07 -0500

    Add Email Sending

--- api_util.py
+++ api_util.py
@@ -1,6 +1,7 @@
 """This file will contain functions related to the API"""
 import mysql.connector
 import smtplib
+import getpass
 
 # Initalizes a connection to the database
 mydb = mysql.connector.connect(
@@ -68,4 +69,53 @@ def add_new_user(new_user_list):
 
 
 def send_email(user_email, trainer_email, injury_form):
-    return ""
+    HOST = "smtp-mail.outlook.com"
+    PORT = 587
+    # users_password = get_users_password(user_email)
+    # if users_password == False:
+    #     return False
+    EMAIL = "studentInjuryTest@outlook.com"
+    PASSWORD = "ASDFGHJKL;z123"
+
+    MESSAGE = f"""Subject: Patient Outcome Reported Meaasure
+        Hi, 
+
+        I finished my injury form! What days can we meet to discuss it?
+        
+        {injury_form}
+    """
+    try:
+        smtp = smtplib.SMTP(HOST, PORT)
+
+        staus_code, response = smtp.ehlo()
+        print(f"Echoing the server:  {staus_code} {response}")
+
+        staus_code, response = smtp.starttls()
+        print(f"Starting tls connection:  {staus_code} {response}")
+
+        staus_code, response = smtp.login(
+            EMAIL, PASSWORD
+        )  # This will need to be the users password
+        print(f"Logging in: {staus_code} {response}")
+
+        smtp.sendmail(EMAIL, trainer_email, MESSAGE)
+        print("Email sent Succesfully!")
+        return True
+    except:
+        print("Error!")
+        return False
+
+
+"""This function will get the users email password from the Database"""
+
+
+def get_users_password(user_email):
+    # Use this video guid for refreence: https://www.youtube.com/watch?v=x7SwgcpACng
+    cursor.execute(f"SELECT * FROM usersdb.userinfo WHERE email is {user_email}")
+    result = (
+        cursor.fetchall()
+    )  # gets all values from select statement and returns a list of tuples
+    try:
+        return result.pop(0)  # Return the first password
+    except:
+        return False  # If Something fails
