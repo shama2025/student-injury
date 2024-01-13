447109e3872f3c7d10f90bbfbd5bba12706d2187
shama2025 <2003mashaffer@gmail.com>
2024-01-13 16:42:44 -0500

    Update Service
    Now service sends a post method and appends all values in the form to formdata object.

--- src/app/services/athletic-tainer-email-service.service.ts
+++ src/app/services/athletic-tainer-email-service.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Form } from '@angular/forms';
+
 @Injectable({
     providedIn: 'root'
 })
 export class AthleticTainerEmailServiceService {
     constructor(private http: HttpClient) {}
     private baseApiUrl = environment.apiBaseUrl;
+    private formData: FormData = new FormData();
+    sendEmail(userEmail: string, trainerEmail: string, injuryForm: File | null | undefined): Observable<any> {
+        this.formData.append('userEmail', userEmail);
+        this.formData.append('trainerEmail', trainerEmail);
 
-    sendEmail(userEmail: String, trainerEmail: String, injuryForm: File | null | undefined): Observable<any> {
-        const emailUrl: string = `${this.baseApiUrl}/api/at/email?userEmail=${userEmail}&trainerEmail=${trainerEmail}&injuryForm=${injuryForm}`;
-        return this.http.get(emailUrl);
+        if (injuryForm) {
+            this.formData.append('injuryForm', injuryForm, injuryForm.name);
+        }
+        const emailUrl: string = `${this.baseApiUrl}/api/at/email`;
+        return this.http.post(emailUrl, this.formData);
     }
 }
